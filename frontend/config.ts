f7a2479b4344f0144b8d0b9d8c6e1b7dde8a3042
Xavier D'Mello <xaviermdmello@gmail.com>
2025-11-23 06:15:02 -0300

    sad

--- frontend/config.ts
+++ frontend/config.ts
@@ -6,7 +6,7 @@ export const config = {
     wagerAmount: "0.01", // 1 cent
   },
   114: { // flare-coston2
-    address: "0x5d98c86ca7dc8e760468984361ba0329702006d4",
+    address: "0x35ae90554500206f6ae53089a0e02574a59bcbad",
     entropyV2Address: "0x41c9e39574f40ad34c79f1c99b66a45efb830d4c",
     usdcAddress: "0x573d34311044b51246d022c326b12cd2dffc82a1",
     wagerAmount: "10", // 1 cent
